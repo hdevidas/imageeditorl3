9a7bf60197371f089316c999f18305f98c0fac74
hdevidas <hugo.devidas@etu.u-bordeaux.fr>
2020-01-31 14:24:55 +0100

    Creation Repo

---
+++ app/src/main/java/l3/techno/imageeditorl3/MainActivity.java
@@ -0,0 +1,14 @@
+package l3.techno.imageeditorl3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
